bb8753dc38d7b1c9069d26d4753271e4662799f4
laea <laea@o2.pl>
2017-11-14 22:13:32 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-function myMap(){var e={center:new google.maps.LatLng(52.237049,21.017532),zoom:10,mapTypeId:google.maps.MapTypeId.ROADMAP};new google.maps.Map(document.getElementById("map"),e)}console.log("plugins");
+function myMap() {
+var mapOptions = {
+    center: new google.maps.LatLng(52.237049, 21.017532),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
