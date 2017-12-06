359b80b149ef9523c9138bb5fc3fbb95878926be
laea <laea@o2.pl>
2017-12-06 04:36:10 +0100

    Add files via upload
    Final version.

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ var mapOptions = {
 }
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+var scroll = new SmoothScroll('a[href*="#"]');
+window.addEventListener('scroll', trans);
+function trans() {
+  if (window.scrollY > 100){
+    document.querySelector('nav').classList.add("nav__fixed")}
+if (window.scrollY < 100) {
+  document.querySelector('nav').classList.remove("nav__fixed")}
+}
+
+function menu() {
+  var x = document.querySelector('ul.nav__list');
+  if( x.style.display === "none") {
+    x.style.display = "block"
+  } else {
+    x.style.display = "none"
+  }
+  event.stopPropagation();
+}
+function delMenu() {
+  document.querySelector('ul.nav__list').style.display = "none";
+}
+document.querySelector('body').addEventListener("click", delMenu);
